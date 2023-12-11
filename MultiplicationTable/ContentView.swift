36f54e3ec839e237f24a6314e530459fa7374997
1e1104k4 <1e1104k4@gmail.com>
2023-12-10 20:01:24 -0600

    picker selection for amount of questions

--- MultiplicationTable/ContentView.swift
+++ MultiplicationTable/ContentView.swift
@@ -10,14 +10,24 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var multiplyBy = 2
+    @State private var selectedAmountOfQuestions = 10
+    let amountOfQuestions = [5, 10, 20]
     
     var body: some View {
-        VStack {
-            Stepper("Number for practice: \(multiplyBy)", value: $multiplyBy, in: 2...12, step: 1)
-        }.padding()
-        
-    }
-}
+        NavigationStack {
+            Form {
+                Stepper("Number for practice: \(multiplyBy)", value: $multiplyBy, in: 2...12, step: 1)
+                Section("Amount of questions") {
+                    Picker("Amount of questions", selection: $selectedAmountOfQuestions) {
+                        ForEach(amountOfQuestions, id: \.self) {
+                            Text($0, format: .number)
+                        }
+                    }.pickerStyle(.segmented)
+                }
+            } // FORM
+        } // NAV STACK
+    } // BODY
+} // STRUCT
 
 #Preview {
     ContentView()
