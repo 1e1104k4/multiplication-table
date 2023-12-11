8d3f246ae1bfb2309a522be563c31ee9ea7ceeb4
1e1104k4 <1e1104k4@gmail.com>
2023-12-10 19:18:09 -0600

    Initial Commit

---
+++ MultiplicationTable/MultiplicationTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTableApp.swift
+//  MultiplicationTable
+//
+//  Created by Leila on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
